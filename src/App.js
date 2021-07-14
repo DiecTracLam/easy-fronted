ebc3c3452e2581702eea05353b9bd720d65e1022
Diec Trac Lam <diectraclam.0812@gmail.com>
2021-07-14 19:07:01 +0700

    Clean

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
-import Countdown from './features/CountDownList';
-import Todo from './Components/Todo';
-import MagicBox from './Components/MagicBox';
-import DualCounter from './Components/TodoList';
-import TodoFeature from './features/ToDo';
-import { useEffect, useState } from 'react';
-import {
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import productApi from './api/productApi';
 
 function App() {
 
-  useEffect(()=>{
-    async function fetchData(){
-      const params={
-        _limit:10
-      }
-      // const productList= await productApi.getAll(params);
-      // console.log(productList)
-    }
-    fetchData()
-  },[])
-
-  const [count,setCount]=useState(false)
-
-  function change(e){
-    setCount(Count=>!Count)
-  }
   return (
     <div className="App">
-        <Link to="/">Home</Link><br/>
-        <Link to="/Countdown">Countdown</Link><br/>
-        <Link to="/Todo">Todo</Link>
-
-        <Route path="/" component={MagicBox}/>
-        <Route path="/Countdown" component={Countdown} />
-        <Route path="/Todo" component={TodoFeature} />
-        <p onClick={change}>do</p>
-        {/* {count &&<Todo/>}
-        <Countdown/>
-        <MagicBox/>
-        <DualCounter/> */}
+  
     </div>
   );
 }
